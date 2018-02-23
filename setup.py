23a295743e187b6281bc326c05b72c6fecba10b8
Andy Balogh <andy.balogh@keysight.com>
2018-02-22 17:43:29 -0800

    new version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.55a53',
+    version='0.55a54',
 
     description='IxNetwork REST API Client',
     long_description=long_description,
